f1588914de694c8e9e5a6c7ecec5809312058541
Toby Hinloopen <toby@kutcomputers.nl>
2011-11-03 22:51:22 +0100

    Further implemented the StringScanner class

--- lib/will_scan_string/string_scanner.rb
+++ lib/will_scan_string/string_scanner.rb
@@ -1,35 +1,42 @@
 module WillScanString
 	class StringScanner
-		def register_replacement( *args, &block )
-			args.push block if block.present?
+		def register_replacement( pattern, replacement )
 			@replacements = [] if @replacements.nil?
-			@replacements << args
+			@replacements << [ pattern, replacement, @replacements.last.present? ? @replacements.last[2] + (@replacements.last[0].is_a?(Regexp) ? @replacements.last[0].capture_groups.length : 1) : 0 ]
 			@replacements_regexp = nil
 		end
 
 		def replace( string )
 			string.gsub(replacement_regexp) do
-				m = $~.to_a.tap{ |m| m.shift }
-				i = m.find_index{ |v| v.present? }
-				m = m[i]
-				r = @replacements[i].last
-				case r
-				when Proc
-					r.call m
-				else
-					r.to_s
-				end
+				m, r = *get_match_and_replacement( $~ )
+				execute_replacement_with_match r, m
 			end
 		end
 
 		protected
+		def get_match_and_replacement( m )
+			m = m.to_a
+			i = m[1..-1].find_index{ |v| v.present? }
+			r = find_replacement_by_index(i)
+			cps = [0] + (r[0].is_a?(Regexp) ? r[0].capture_groups : [])
+			m = Hash[ cps.zip m[i, cps.length] ]
+			[m, r[1]]
+		end
+
+		def execute_replacement_with_match( r, m )
+			r.is_a?(Proc) ? r.call(m) : r.to_s
+		end
+
+		def find_replacement_by_index( i )
+			@replacements.find{ |r| r[2] == i }
+		end
+
 		def replacement_regexp
 			@replacements_regexp ||= reconstruct_replacement_regexp
 		end
 
 		private
 		def reconstruct_replacement_regexp
-			c = -1
 			pattern = @replacements.map(&:first).map{ |pat|
 				"(#{ pat.is_a?(Regexp) ? pat.source : Regexp.escape(pat.to_s) })"
 			}.join "|"
