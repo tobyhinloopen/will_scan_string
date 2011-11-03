7cdece9408c4fd80fc6a6941a1c8267cfcb8fadc
Toby Hinloopen <toby@kutcomputers.nl>
2011-11-03 22:51:35 +0100

    Improved and added tests for StringScanner class

--- spec/lib/will_scan_string/string_scanner_spec.rb
+++ spec/lib/will_scan_string/string_scanner_spec.rb
@@ -1,10 +1,27 @@
 require 'spec_helper.rb'
 
 describe WillScanString::StringScanner do
-	it "dunno" do
+	it "should perform a regular replacement" do
 		ss = WillScanString::StringScanner.new
-		ss.register_replacement ":)", %{HAPPY}
-		ss.register_replacement ":(", %{SAD}
-		ss.replace(":) or :(").should eql("HAPPY or SAD")
+		ss.register_replacement ":)", "HAPPY"
+		ss.replace(":)").should eql "HAPPY"
+	end
+
+	it "should pass on named capture groups in a regexp" do
+		ss = WillScanString::StringScanner.new
+		ss.register_replacement %r{<(?<tagname>[a-z]+)>.*?</\k<tagname>>}, ->(m){
+			m.should eql 0 => "<strong>hi!</strong>", :tagname => "strong"
+		}
+		ss.replace "<strong>hi!</strong>"
+	end
+
+	it "should not replace replaced strings" do
+		ss = WillScanString::StringScanner.new
+		ss.register_replacement ":)", %{<img src="happy.png" alt=":)" title=":)">}
+		ss.register_replacement "<", "&lt;"
+		ss.register_replacement ">", "&gt;"
+		ss.register_replacement "\"", "&quot;"
+		ss.register_replacement "&", "&amp;"
+		ss.replace("& :)").should eql %{&amp; <img src="happy.png" alt=":)" title=":)">}
 	end
 end
